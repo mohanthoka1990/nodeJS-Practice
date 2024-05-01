16164e09a3fe036e5ac580e5f6fc07a176995ba7
mohanthoka1990 <mohan.mona9966@gmail.com>
2024-05-01 16:21:42 +0530

    new changes updated

--- myApp/app.js
+++ myApp/app.js
@@ -1,8 +1,23 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.get('/gadgets', (request, response) => {
-  response.sendFile('./gadgets.html', {root: __dirname})
+// app.get('/gadgets', (request, response) => {
+//   response.sendFile('./gadgets.html', {root: __dirname})
+// })
+
+// module.exports = app;
+const express = require('express')
+const addDays = require('date-fns/addDays')
+const app = express()
+app.get('/', (request, response) => {
+  const dateTime = new Date()
+  const result = addDays(
+    new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate()),
+    100,
+  )
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`,
+  )
 })
 
-module.exports = app;
+module.exports = app
